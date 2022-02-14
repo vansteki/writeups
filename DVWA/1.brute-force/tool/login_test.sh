00d590b3271f1775e8eac2e4b0fec5c01728b609
vansteki <vansteki.tw@gmail.com>
2022-02-14 17:31:48 +0800

    refactor: update host variables and reformat

--- DVWA/1.brute-force/tool/login_test.sh
+++ DVWA/1.brute-force/tool/login_test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # DVWA host
-HOST=http://localhost:8086/login.php
+HOST=http://localhost:8086
 # create cookie and save csrf token
-CSRF=`curl -s -L -c cookies.txt -b cookies.txt "http://localhost:8086/login.php" | grep user_token  | grep -oE \[a-z0-9\]+ | awk 'length >= 10'`
+CSRF=`curl -s -L -c cookies.txt -b cookies.txt "${HOST}/login.php" | grep user_token  | grep -oE \[a-z0-9\]+ | awk 'length >= 10'`
 # save session id
 SESSIONID=$(grep PHPSESSID cookies.txt | cut -d $'\t' -f7)
 
@@ -12,12 +12,12 @@ LOGIN_PASSWORD=password
 
 # login at login page
 curl -v -L -c cookies.txt -b cookies.txt \
--X POST "http://localhost:8086/login.php" \
+-X POST "${HOST}/login.php" \
 --data-raw \
 "username=${USER}&password=${LOGIN_PASSWORD}&Login=Login&user_token=${CSRF}"
 
 # enter username and password in brute force page: /vulnerabilities/brute/
-RES=`curl -L -v -c cookies.txt -b cookies.txt "http://localhost:8086/vulnerabilities/brute/?username=${USER}&password=${LOGIN_PASSWORD}&Login=Login#"`
+RES=`curl -L -v -c cookies.txt -b cookies.txt "${HOST}/vulnerabilities/brute/?username=${USER}&password=${LOGIN_PASSWORD}&Login=Login#"`
 
 if [[ $RES == *"Welcome to the password protected area admin"* ]]; then
   printf "\n PASS!✅ \n"

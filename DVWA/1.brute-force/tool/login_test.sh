05720ef9ea0b8e58bcc95515758a8727044f3af1
vansteki <vansteki.tw@gmail.com>
2022-02-14 17:20:07 +0800

    refactor: if test ok it will show PASS!✅

--- DVWA/1.brute-force/tool/login_test.sh
+++ DVWA/1.brute-force/tool/login_test.sh
@@ -8,7 +8,16 @@ USER=admin
 LOGIN_PASSWORD=password
 
 # login at login page
-curl -v -L -c cookies.txt -b cookies.txt -X POST "http://localhost:8086/login.php" --data-raw "username=${USER}&password=${LOGIN_PASSWORD}&Login=Login&user_token=${CSRF}"
+curl -v -L -c cookies.txt -b cookies.txt \
+-X POST "http://localhost:8086/login.php" \
+--data-raw \
+"username=${USER}&password=${LOGIN_PASSWORD}&Login=Login&user_token=${CSRF}"
 
 # enter username and password in brute force page: /vulnerabilities/brute/
-curl -L -v -c cookies.txt -b cookies.txt "http://localhost:8086/vulnerabilities/brute/?username=${USER}&password=${LOGIN_PASSWORD}&Login=Login#"
+RES=`curl -L -v -c cookies.txt -b cookies.txt "http://localhost:8086/vulnerabilities/brute/?username=${USER}&password=${LOGIN_PASSWORD}&Login=Login#"`
+
+#echo $RES | grep admin
+
+if [[ $RES == *"Welcome to the password protected area admin"* ]]; then
+  echo "\n PASS!✅ \n"
+fi

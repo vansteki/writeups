1569201a32bf5fcdf54de575a8c40e1719855397
vansteki <vansteki.tw@gmail.com>
2022-02-14 17:12:43 +0800

    feat: add test scripts

---
+++ DVWA/1.brute-force/tool/login_test.sh
@@ -0,0 +1,14 @@
+# create cookie and save csrf token
+CSRF=`curl -s -L -c cookies.txt -b cookies.txt "http://localhost:8086/login.php" | grep user_token  | grep -oE \[a-z0-9\]+ | awk 'length >= 10'`
+# save session id
+SESSIONID=$(grep PHPSESSID cookies.txt | cut -d $'\t' -f7)
+
+# user & password for login to start tasks
+USER=admin
+LOGIN_PASSWORD=password
+
+# login at login page
+curl -v -L -c cookies.txt -b cookies.txt -X POST "http://localhost:8086/login.php" --data-raw "username=${USER}&password=${LOGIN_PASSWORD}&Login=Login&user_token=${CSRF}"
+
+# enter username and password in brute force page: /vulnerabilities/brute/
+curl -L -v -c cookies.txt -b cookies.txt "http://localhost:8086/vulnerabilities/brute/?username=${USER}&password=${LOGIN_PASSWORD}&Login=Login#"

